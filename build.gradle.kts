51bd2cfbabe8b830abde13c4e9c0a5287ecab9d6
pigedy100 <ruhosk@gmail.com>
2025-01-02 20:34:25 -0600

    modified:   build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
 	id("dev.frozenmilk.teamcode") version "10.1.1-0.1.3"
 }
-
+dependencies {
+	implementation("dev.frozenmilk.mercurial:Mercurial:1.0.2")
+	implementation("dev.frozenmilk.dairy:Core:1.0.1")
+	implementation("dev.frozenmilk.dairy:Util:1.1.0")
+}
 ftc {
 	// use this to easily add more FTC libraries
-
 	// adds support for kotlin
 	kotlin
 }

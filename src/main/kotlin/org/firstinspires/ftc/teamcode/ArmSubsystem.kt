bcbb9cb0fa7b688757e25258d4c70f6427b24a1c
pigedy100 <ruhosk@gmail.com>
2025-01-16 23:53:46 -0600

    set up commands

--- src/main/kotlin/org/firstinspires/ftc/teamcode/ArmSubsystem.kt
+++ src/main/kotlin/org/firstinspires/ftc/teamcode/ArmSubsystem.kt
@@ -10,6 +10,7 @@ import dev.frozenmilk.dairy.core.FeatureRegistrar
 import dev.frozenmilk.dairy.core.dependency.Dependency
 import dev.frozenmilk.dairy.core.dependency.annotation.SingleAnnotation
 import dev.frozenmilk.dairy.core.wrapper.Wrapper
+import dev.frozenmilk.mercurial.commands.Lambda
 import dev.frozenmilk.mercurial.subsystems.Subsystem
 import java.lang.annotation.Inherited
 import kotlin.math.cos
@@ -41,32 +42,35 @@ object ArmSubsystem : Subsystem {
     var wristPos: Double = 0.0
     var clawPos: Double = 0.0
     var armAngle: Double = -45.0
-    @JvmField var clawScale: Double = .3
-    @JvmField var wristScale: Double = .48
-    @JvmField var wristOffset: Double = .41
+    var clawScale: Double = .3
+    var wristScale: Double = .48
+    var wristOffset: Double = .41
+    @JvmField var maxArmAngle: Double = 220.0
 
     //PIDF Coefficients
     @JvmField var p = 0.005
     @JvmField var i = 0.0
     @JvmField var d = 0.0003
     @JvmField var f = 0.1
-    @JvmField var ticksPerDegree = 500.0/135.0
-    @JvmField var targetArmPosition = 0.0
-    @JvmField var angleOffset = -45.0
+    var ticksPerDegree = 500.0/135.0
+    @JvmField var targetArmAngle = 45.0
+    var angleOffset = -45.0
 
     var pidController: PIDController = PIDController(p,i,d)
 
+    //loop
     override fun preUserLoopHook(opMode: Wrapper) {
-        pidController.setPID(p,i,d)
-        var armTicks = motor.currentPosition
-        armAngle = (armTicks/ticksPerDegree) + angleOffset
-        var result = pidController.calculate(armTicks.toDouble(), targetArmPosition)
-        var ff= cos(Math.toRadians((targetArmPosition/ticksPerDegree) + angleOffset)) * f
-        var reactiveFeedforward= cos(Math.toRadians(armAngle)) * f
+        targetArmAngle =  if(targetArmAngle > maxArmAngle) maxArmAngle else targetArmAngle //calculate target angle, limit if it would make the arm go over the maximum
+        pidController.setPID(p,i,d) //i feel like this could go in an init or something now, but when tuning this needs to be here
+        var armTicks = motor.currentPosition //could probably be cached or something
+        armAngle =(armTicks/ticksPerDegree) + angleOffset
+        var result = pidController.calculate(armAngle, targetArmAngle)
+        var ff= cos(Math.toRadians(targetArmAngle))*f
+        var reactiveFeedforward = cos(Math.toRadians(armAngle)) * f
         opMode.opMode.telemetry
             .addData("pos", armTicks)
             .addData("angle", armAngle)
-            .addData("target pos", targetArmPosition)
+            .addData("target angle", targetArmAngle)
             .addData("arm power", motorPwr)
             .addData("wrist pos", wristPos)
             .addData("claw pos", clawPos)
@@ -75,7 +79,7 @@ object ArmSubsystem : Subsystem {
         var dashboardTelemetry = dashboard.getTelemetry();
 
         dashboardTelemetry.addData("real", armTicks)
-        dashboardTelemetry.addData("target", targetArmPosition)
+        dashboardTelemetry.addData("target", targetArmAngle)
         dashboardTelemetry.addData("ff", ff);
         dashboardTelemetry.addData("result", result);
 
@@ -84,14 +88,104 @@ object ArmSubsystem : Subsystem {
         claw.position=clawPos*clawScale
     }
 
+    //BASIC METHODS
     fun setArmVals(a: Double, w: Double, c: Double){
-        targetArmPosition+=a
+        targetArmAngle=a
         wristPos=w
         clawPos=c
+    }
+
+    fun setArmVals(a: Double, w: Double){
+        targetArmAngle=a
+        wristPos=w
+    }
+
+    fun setArmVals(vals: Vals){
+        setArmVals(vals.arm,vals.wrist)
     }
 
     fun resetLocation(){
         motor.mode= DcMotor.RunMode.STOP_AND_RESET_ENCODER
         motor.mode= DcMotor.RunMode.RUN_WITHOUT_ENCODER
     }
+
+
+    //COMMANDS
+    @JvmField var frontFloorVals = Vals(-30.0, 1.0)
+    val frontFloor = Lambda("front floor ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(frontFloorVals)
+        }
+
+    @JvmField var frontWallVals = Vals(10.0, 0.2)
+    val frontWall = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(frontWallVals)
+        }
+
+    @JvmField var backWallVals = Vals(170.0, 0.2)
+    val backWall = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(backWallVals)
+        }
+
+    @JvmField var backFloorVals = Vals(220.0, 0.2)
+    val backFloor = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(backFloorVals)
+        }
+
+    @JvmField var maxFrontVals = Vals(0.0, 0.2)
+    val maxFront = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(maxFrontVals)
+        }
+
+    @JvmField var maxBackVals = Vals(180.0, 0.2)
+    val maxBack = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(maxBackVals)
+        }
+    
+    @JvmField var highBarVals = Vals(180.0, 0.2)
+    val highBar = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(highBarVals)
+        }
+
+    @JvmField var lowBarVals = Vals(180.0, 0.2)
+    val lowBar = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(lowBarVals)
+        }
+
+    @JvmField var basketVals = Vals(180.0, 0.2)
+    val basket = Lambda("wall ")
+        .setInterruptible(true)
+        .setInit{
+            setArmVals(lowBarVals)
+        }
+
+    //claw commands
+    val closeClaw = Lambda("close claw")
+        .setInterruptible(true)
+        .setInit{
+            clawPos = 1.0
+        }
+
+    val openClaw = Lambda("open claw")
+        .setInterruptible(true)
+        .setInit{
+            clawPos = 0.0
+        }
+
+    data class Vals(val arm: Double, val wrist: Double)
 }
--- src/main/kotlin/org/firstinspires/ftc/teamcode/BasicOPMode.kt
+++ src/main/kotlin/org/firstinspires/ftc/teamcode/BasicOPMode.kt
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode
 
 import com.acmerobotics.dashboard.FtcDashboard
 import com.acmerobotics.dashboard.config.Config
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import dev.frozenmilk.dairy.pasteurized.SDKGamepad
+import dev.frozenmilk.dairy.pasteurized.layering.LayeredGamepad
+import dev.frozenmilk.dairy.pasteurized.layering.MapLayeringSystem
 import dev.frozenmilk.mercurial.Mercurial
 import dev.frozenmilk.mercurial.bindings.BoundGamepad
+import dev.frozenmilk.mercurial.commands.Lambda
 
 @Mercurial.Attach
 @ArmSubsystem.Attach
@@ -15,23 +17,84 @@ import dev.frozenmilk.mercurial.bindings.BoundGamepad
 @Config
 @TeleOp
 class BasicOPMode : OpMode() {
+    var t:Double = 0.0
+    enum class Layers {
+        INTAKING,
+        DEPOSITING,
+        DEFAULT
+    }
+
+    val intakingGamepad = BoundGamepad(SDKGamepad(gamepad1))
+    val depositingGamepad = BoundGamepad(SDKGamepad(gamepad1))
+    val defaultGamepad = BoundGamepad(SDKGamepad(gamepad1))
+
+    val gamepadMap = mapOf(
+        Layers.INTAKING to intakingGamepad,
+        Layers.DEPOSITING to depositingGamepad,
+        Layers.DEFAULT to defaultGamepad
+    )
+
+    val enumLayeringSystem = MapLayeringSystem(Layers.DEFAULT, gamepadMap)
+    val layeredGamepad = LayeredGamepad(enumLayeringSystem)
+
+    val defaultLayerTransition  = Lambda("default layer transition")
+        .setInit{ enumLayeringSystem.layer = Layers.DEFAULT}
+
     override fun init() {
-        val boundGamepad = BoundGamepad(SDKGamepad(gamepad1))
+        layeredGamepad.leftStickButton
+            .onTrue (Lambda("intaking layer transition")
+                .setInit{ enumLayeringSystem.layer = Layers.INTAKING}
+            )
+            .onFalse (defaultLayerTransition)
+        layeredGamepad.rightStickButton
+            .onTrue (Lambda("depositing layer transition")
+                .setInit{enumLayeringSystem.layer = Layers.DEPOSITING})
+            .onFalse (defaultLayerTransition)
+
+
+        intakingGamepad.leftTrigger.conditionalBindState()
+            .greaterThan(0.1).bind()
+            .onTrue(ArmSubsystem.frontFloor)
+        intakingGamepad.leftBumper
+            .onTrue(ArmSubsystem.frontWall)
+        intakingGamepad.rightTrigger.conditionalBindState()
+            .greaterThan(0.1).bind()
+            .onTrue(ArmSubsystem.backFloor)
+        intakingGamepad.rightBumper
+            .onTrue(ArmSubsystem.backWall)
+        
+        depositingGamepad.leftTrigger.conditionalBindState()
+            .greaterThan(0.1).bind()
+            .onTrue(ArmSubsystem.maxBack)
+        depositingGamepad.leftBumper
+            .onTrue(ArmSubsystem.highBar)
+        depositingGamepad.rightTrigger.conditionalBindState()
+            .greaterThan(0.1).bind()
+            .onTrue(ArmSubsystem.maxFront)
+        depositingGamepad.rightBumper
+            .onTrue(ArmSubsystem.lowBar)
+        depositingGamepad.leftTrigger.conditionalBindState()
+            .greaterThan(.1).bind()
+            .and(
+                depositingGamepad.rightTrigger.conditionalBindState()
+                    .greaterThan(0.1).bind()
+            )
+            .onTrue(ArmSubsystem.basket)
+
+        defaultGamepad.rightBumper
+            .onTrue(ArmSubsystem.openClaw)
+        defaultGamepad.leftBumper
+            .onTrue(ArmSubsystem.openClaw)
     }
 
     override fun loop() {
+        t+=gamepad2.right_stick_y.toDouble()
         DrivetrainSubsystem.setDrivePower(
             gamepad1.left_stick_x.toDouble(),
             -gamepad1.left_stick_y.toDouble(),
             gamepad1.right_stick_x.toDouble()
         )
-        ArmSubsystem.setArmVals(
-            gamepad1.right_stick_y.toDouble(),
-            gamepad1.right_trigger.toDouble(),
-            gamepad1.left_trigger.toDouble()
-        )
-
-        if(gamepad1.start) ArmSubsystem.resetLocation()
+        if(gamepad1.start || gamepad1.options) ArmSubsystem.resetLocation()
 
         var dashboard = FtcDashboard.getInstance();
         var dashboardTelemetry = dashboard.getTelemetry();
